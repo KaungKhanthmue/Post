b8a42770c8fc8485328c15984719a80509276eea
kaungkhanthmue <kaungkhanthmue58@gmail.com>
2024-10-06 01:32:38 +0630

    add ui

--- src/app/page/chat/page.tsx
+++ src/app/page/chat/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+import { CirclePlus,ImagePlus,Paperclip,Send } from 'lucide-react'
 
-function page() {
+export default function Page() {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex w-full gap-2 p-1 ">
+      <div className="w-[80%] h-[830px] bg-black border relative ">
+        <div className="w-[98%] h-24   absolute bottom-0  flex items-center gap-2 justify-center">
+        <CirclePlus width={35} height={35} className='ml-2'/>
+        <ImagePlus width={35} height={35} />
+        <Paperclip width={35} height={35} />
+        <input type="text" className='bg-black border w-[84%] h-10 rounded-full pl-4' />
+        <Send width={35} height={35} />
+        </div>
+      </div>
 
-export default page
+      {/* active user */}
+      <div className="w-[20%] h-[825px] bg-black border ">
+        <div className="p-1">
+          <div className="w-full  h-14 border"></div>
+        </div>
+        <div className="flex justify-center w-full h-14 mb-2">
+          <div className="flex items-center gap-2 w-[290px] rounded-md ">
+            <div className="w-10 h-10 rounded-full bg-red-200 ml-1"></div>
+            <div className="hidden lg:block">Kaung Khant Hmue</div>
+          </div>
+        </div>
+        <div className="flex justify-center w-full h-14 ">
+          <div className="flex items-center gap-2 w-[290px] rounded-md">
+            <div className="w-10 h-10 rounded-full bg-red-300 ml-1"></div>
+            <div className="hidden lg:block">Zin May Htet </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page/layout.tsx
+++ src/app/page/layout.tsx
@@ -18,8 +18,8 @@ export default function PageLayout({
 
   return (
     <section>
-      <div className="grid w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex w-full">
+        <div className=" border-r w-[15%]">
           <div className="flex flex-col gap-2">
             <div className="flex h-14 items-center border-b lg:h-[60px] justify-between pr-1">
               <div className="w-[100px] bg-black h-full ml-1"></div>
@@ -35,7 +35,7 @@ export default function PageLayout({
           </div>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
           <Header />
         </div>
       </div>
@@ -43,8 +43,8 @@ export default function PageLayout({
       <div className="flex mt-1">
         <div
           className={`fixed top-15 left-0 h-screen bg-black border transition-transform transform ${
-            isDrawerOpen ? "translate-x-0" : "-translate-x-full"
-          } w-[280px] rounded-md`}
+            isDrawerOpen ? "translate-x-0" : "-translate-x-full" 
+          } w-[270px] rounded-md`}
         >
           <div className="p-4">
             <Link href="/another-page">
@@ -58,7 +58,7 @@ export default function PageLayout({
           </div>
         </div>
 
-        <div className={`transition-all ${isDrawerOpen ? "ml-[280px]" : "ml-0"}`}>
+        <div className={`w-full transition-all ${isDrawerOpen ? "ml-[270px]" : "ml-0"}`}>
           {children}
         </div>
       </div>
